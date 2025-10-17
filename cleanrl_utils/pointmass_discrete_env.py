fb2369db89e7e81bcf9c48b9b4c064f5fce1e1a1
apoorva <mittalapoorva1999@gmail.com>
2025-10-17 16:09:56 -0500

    Added reward and action in discrete_env

--- cleanrl_utils/pointmass_discrete_env.py
+++ cleanrl_utils/pointmass_discrete_env.py
@@ -27,24 +27,57 @@ class PointMassDiscreteEnv(PointMassContinuousEnv):
 
         # TODO: Decide how many discrete actions you need.
         # Action space: some number of discrete actions
-        self.action_space = spaces.Discrete(4)
+        self.action_space = spaces.Discrete(5)
 
         # Initialize with random positions
         self._reset_positions()
 
     def step(self, action):
         assert self.action_space.contains(action)
+        action_to_control = {
+            0: (np.array([ self.max_velocity,  0.0]), "right"),  # Right
+            1: (np.array([- self.max_velocity,  0.0]), "left"),  # Left
+            2: (np.array([ 0.0,  self.max_velocity]), "up"),  # Up
+            3: (np.array([ 0.0, -self.max_velocity]), "down"),  # Down
+            4: (np.array([ 0.0,  0.0]), "no-op"),  # No-op
+        }
 
         # TODO: TURN DISCRETE ACTIONS INTO A CONTINUOUS ONE.
-        control = np.ones(2)  # placeholder logic
+        control = action_to_control[action][0] # placeholder logic
+
+
+        # Debug: Print before calling parent step
+
+        prev_distance = np.linalg.norm(self._agent_pos - self._goal_pos)
+        steps = self._steps
+
+        if steps % 100 == 0:
+            print(f"DEBUG: Before step - Agent pos: {self._agent_pos}, move: {action_to_control[action][1]}, goal pos: {self._goal_pos}, distance: {prev_distance}")
 
         _, _, terminated, truncated, info = super().step(control)
         observation = self._get_obs()
-
-        # TODO: DEFINE THE REWARD SIGNAL
-        reward = 0.0
+        new_distance = np.linalg.norm(self._agent_pos - self._goal_pos)
+        reward = self.get_reward(prev_distance, new_distance, terminated)
+        # reward -= 0.1  
+        if steps % 100 == 0:
+            print(f"DEBUG: After step - Agent pos: {self._agent_pos}, goal pos: {self._goal_pos}, distance: {new_distance}, reward: {reward}")
+       
 
         return observation, reward, terminated, truncated, info
+
+    def get_reward(self, prev_distance, new_distance, terminated):
+        progress = (prev_distance - new_distance) 
+        reward = progress * 100.0
+        reward -= 0.05
+        collided = self._check_collision(self._agent_pos, self.agent_radius)
+        out_of_bounds = self._check_out_of_bounds(self._agent_pos, self.agent_radius)
+        if collided or out_of_bounds:
+            reward -= 2.0  # collision penalty
+        if terminated and new_distance < self.goal_radius:
+            reward += 100.0  # goal bonus
+
+        return reward
+
 
     def _get_obs(self) -> np.ndarray:
         """Get normalized observation vector."""
